00b0fc1e9ac13079e8fc0ebb86cf818944b596e8
davydes <davydes@gmail.com>
2015-10-19 15:27:01 +0300

    i18n supported

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Gym
     config.time_zone = 'Europe/Moscow'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
