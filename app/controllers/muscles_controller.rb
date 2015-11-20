1de859bbbca729089aba05f15657fa4cc1c90a58
davydes <davydes@gmail.com>
2015-11-20 13:25:33 +0300

    list of muscles devided by bodyparts

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,4 +1,9 @@
 class MusclesController < AnatomicController
+
+  def index
+    @body_parts = BodyPart.joins(:muscles).group('body_parts.id').having('count( body_part_id ) > 0').order(:name)
+  end
+
   private
 
   def resource_params
