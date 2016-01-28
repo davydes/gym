35f0537c4e9d588d0bde1a74fbab450e3fa3249d
davydes <davydes@gmail.com>
2016-01-28 15:16:21 +0300

    remove images upload from main muscle form

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,13 +1,5 @@
 class MusclesController < AnatomicController
 
-  def create
-    @muscle = Muscle.new(resource_params)
-    params[:pictures]['image'].each do |i|
-      @picture = @muscle.pictures.create!(:image => i)
-    end if @muscle.save
-    respond_with @muscle
-  end
-
   def index
     @body_parts = BodyPart.with_muscles.order(:name)
     @muscles = Muscle.no_bodypart
@@ -16,7 +8,7 @@ class MusclesController < AnatomicController
   private
 
   def resource_params
-    accessible = [ :name, :alias, :description, :shape, :body_part_ids => [], :exercise_ids  => [], pictures_attributes: [:image] ]
+    accessible = [ :name, :alias, :description, :shape, :body_part_ids => [], :exercise_ids  => [] ]
     params.require(:muscle).permit(accessible)
   end
 end
