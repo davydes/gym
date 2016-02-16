f416900672e27d291b6e90a661220e4725266533
davydes <davydes@gmail.com>
2016-02-16 13:43:37 +0300

    remove unused method from ActionController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def ensure_signup_complete
-    return if action_name == 'finish_signup'
-    if current_user && !current_user.email_verified?
-      redirect_to finish_signup_path(current_user)
-    end
-  end
-
   protected
 
   def configure_permitted_parameters
