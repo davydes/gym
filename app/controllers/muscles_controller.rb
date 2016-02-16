1bec25c1e0ff71c9604fa84f3c00dad1a881e0b0
davydes <davydes@gmail.com>
2016-02-16 14:28:33 +0300

    Drop ability to create pictures from MuscleController#create

--- app/controllers/muscles_controller.rb
+++ app/controllers/muscles_controller.rb
@@ -1,6 +1,4 @@
 class MusclesController < AnatomicController
-  after_filter :save_pictures, only: [:create, :update]
-
   def index
     @body_parts = BodyPart.with_muscles.order(:name)
     @muscles = Muscle.no_bodypart
@@ -11,13 +9,5 @@ class MusclesController < AnatomicController
   def resource_params
     accessible = [ :name, :alias, :description, :shape, :body_part_ids => [], :exercise_ids  => [], :pictures_attributes => [] ]
     params.require(:muscle).permit(accessible)
-  end
-
-  def save_pictures
-    if @muscle.save && params[:pictures]
-      params[:pictures]['image'].each do |a|
-        @picture = @muscle.pictures.create!(:name => a.original_filename, :image => a)
-      end
-    end
   end
 end
