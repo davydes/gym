742ae53a30414778630e9821bd022435ee4fe4a7
davydes <davydes@gmail.com>
2016-02-16 14:46:45 +0300

    add triangle shape to Muscles::Shape

--- lib/muscles/shape.rb
+++ lib/muscles/shape.rb
@@ -1,6 +1,6 @@
 module Muscles
   class Shape < Utils::Enum
     self.i18n_scope = 'muscles.shapes'
-    self.valid_values = %w(long short wide diamond square circle)
+    self.valid_values = %w(long short wide diamond square circle triangle)
   end
 end
