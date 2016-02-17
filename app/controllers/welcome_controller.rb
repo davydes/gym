65686d7aaa51fe57c45ae82bc290b8d87eb4e9f2
davydes <davydes@gmail.com>
2016-02-17 13:02:09 +0300

    change Welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @user_count = User.count
+    @muscle_count = Muscle.count
+    @body_part_count = BodyPart.count
+    @exercise_count = Exercise.count
   end
 end
