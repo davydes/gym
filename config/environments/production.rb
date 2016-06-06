fcc9eb4e7fa31bae27c59b164963794a0da3beb2
davydes <davydes@gmail.com>
2016-06-06 13:42:34 +0300

    thrade safe sttings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  # Threadesafe mode
   config.eager_load = true
+  config.preload_frameworks = true
+  config.cache_classes      = true
+  config.dependency_loading = false
+  config.allow_concurrency  = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
