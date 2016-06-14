30bb499992b7e993c98c944dbff29c0a678fd77f
davydes <davydes@gmail.com>
2016-06-14 12:01:55 +0300

    change store Workout::Item.sets form Hash to Array

--- app/models/workout/item.rb
+++ app/models/workout/item.rb
@@ -2,7 +2,7 @@ class Workout::Item < ActiveRecord::Base
   belongs_to :workout
   belongs_to :exercise
 
-  serialize :sets, Hash
+  serialize :sets, Array
 
   validates :workout, :exercise, :sets, :pos, presence: true
   validates :pos, numericality: { only_integer: true, greater_than: 0 },
--- spec/factories/workout_items.rb
+++ spec/factories/workout_items.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :workout_item, class: 'Workout::Item' do
-    sets { { 1 => [160,2], 2 => [170,2]} }
+    sets { [ [160,2], [170,2] ] }
     description 'Workout Item Description'
     pos { workout.items.count + 1 }
   end
