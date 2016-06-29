b6b20008bc407d545f7d309c42ac124a37814246
davydes <davydes@gmail.com>
2016-06-29 12:08:22 +0300

    Back: add rspec for StatisticsMailer

--- spec/mailers/statistics_mailer_spec.rb
+++ spec/mailers/statistics_mailer_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe StatisticsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'StatisticsMailer.common' do
+    before :context do
+      create_list(:admin, 3)
+    end
+    let(:mail) { described_class.common.deliver_now }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Common Stats')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq(User.all.where(admin: true).pluck(:email))
+    end
+  end
 end
