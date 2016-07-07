5dc1c6870897aba660a1f431a3f4007e032ca012
davydes <davydes@gmail.com>
2016-07-07 10:28:48 +0300

    fix references views

--- app/serializers/references/body_part_serializer.rb
+++ app/serializers/references/body_part_serializer.rb
@@ -1,6 +1,6 @@
 class References::BodyPartSerializer < ActiveModel::Serializer
   attributes :id, :name, :alias, :description
 
-  has_many :muscles, serializer: MuscleShortSerializer
-  has_many :exercises, serializer: ExerciseShortSerializer
+  has_many :muscles, serializer: References::MuscleShortSerializer
+  has_many :exercises, serializer: References::ExerciseShortSerializer
 end
--- app/serializers/references/muscle_serializer.rb
+++ app/serializers/references/muscle_serializer.rb
@@ -1,7 +1,7 @@
 class References::MuscleSerializer < ActiveModel::Serializer
   attributes :id, :name, :alias, :description, :shape
 
-  has_many :exercises, serializer: ExerciseShortSerializer
-  has_many :body_parts, serializer: BodyPartShortSerializer
+  has_many :exercises, serializer: References::ExerciseShortSerializer
+  has_many :body_parts, serializer: References::BodyPartShortSerializer
   has_many :pictures
 end
