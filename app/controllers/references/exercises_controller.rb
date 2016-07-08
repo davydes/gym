9d13a644e4f78676c1b9b1d03fd3b0ab828c7e35
davydes <davydes@gmail.com>
2016-07-08 16:14:50 +0300

    Back: add equipment to exercises controller&views

--- app/controllers/references/exercises_controller.rb
+++ app/controllers/references/exercises_controller.rb
@@ -2,7 +2,7 @@ class References::ExercisesController < AnatomicController
   private
 
   def resource_params
-    accessible = [ :name, :alias, :description, :body_part_ids => [], :muscle_ids  => [] ]
+    accessible = [ :name, :alias, :description, :body_part_ids => [], :muscle_ids  => [], :equipment_ids  => [] ]
     params.require(:exercise).permit(accessible)
   end
 end
