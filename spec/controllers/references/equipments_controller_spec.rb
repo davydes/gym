89934da2d86dc8e51660ef305762dacfe68625a0
davydes <davydes@gmail.com>
2016-08-08 11:26:21 +0300

    add tests for controllers

--- spec/controllers/references/equipments_controller_spec.rb
+++ spec/controllers/references/equipments_controller_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe References::EquipmentsController, type: :controller do
-  describe "an example" do
-    it "is implemented but waiting" do
-      pending "something else getting finished"
-      raise "this is the failure"
+  let (:equipments) { create_list(:equipment, 5) }
+
+  context 'JSON' do
+    before :each do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+    end
+
+    describe 'GET #index' do
+      describe 'responds equipments as json with' do
+        it 'right count' do
+          count = equipments.count
+          get :index
+          json = JSON.parse(response.body)
+          expect(json.length).to eq(count)
+        end
+
+        it 'right format' do
+          create(:equipment)
+          get :index
+          json = JSON.parse(response.body)
+          expect(json.first.keys).to eq(%w(id name alias))
+        end
+      end
     end
   end
 end
